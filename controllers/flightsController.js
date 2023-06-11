de4b36bcd048a30b65f16759c7d9d396352db9a2
amos zohar <azamoszohar@gmail.com>
2023-06-11 15:49:08 +0300

    made some style changes

--- controllers/flightsController.js
+++ controllers/flightsController.js
@@ -1,5 +1,33 @@
 const flightDbService = require('../services/flightDbService');
 
+const mockReq = async () => {
+    const mockData = {
+        title:"Flight EA-123",
+        price:100,
+        company:'El Al',
+        origin:'Ben Gurion Airport, Tel Aviv, Israel',
+        destination:'JFK Inernational Airport, New York City, New York',
+        departTime:'23/06/2023,10:00',
+        estimatedTimeOfArrival:'23/06/2023,15:00'
+    }
+    const url = "http://localhost:3000/api/flights";
+    const result = await fetch(url,{
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(mockData), // body data type must match "Content-Type" header
+    });
+    return result;
+};
+
+
 const createFlight = async (req,res) => {
     const newFlight = await flightDbService.createFlight(req.body.data);
     res.json(newFlight);
--- routes/flightsRouter.js
+++ routes/flightsRouter.js
@@ -8,12 +8,12 @@ const {
     deleteFlight//Delete
  } = require('../controllers/flightsController');
 
-flightsRouter.post('/',createFlight);
-flightsRouter.get('/',getFlights);
-flightsRouter.route('/:id')
-    .get(getFlightById)
-    .put(updateFlightData)
-flightsRouter.get('/:filterOBj',getFlightsByParamaters);
-flightsRouter.delete('/:id',deleteFlight);
+//flightsRouter.post('/',createFlight);
+flightsRouter.get('/',(req,res)=>res.send("blabla"));
+//flightsRouter.route('/:id')
+//    .get(getFlightById)
+//    .put(updateFlightData)
+//flightsRouter.get('/:filterOBj',getFlightsByParamaters);
+//flightsRouter.delete('/:id',deleteFlight);
 
 module.exports = flightsRouter;
