17a2e0bc5c84423d805377a91931a5e05ccc088a
amos zohar <azamoszohar@gmail.com>
2023-06-26 12:24:27 +0300

    aded bcrypt hashing, and some basic backend login and register validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,52 @@
 const userDbService = require('../services/userDbService');
+const bcrypt = require('bcrypt');
+const salt_rounds = 12;
+
+const filtered_user = raw_user =>{
+    const { email, display_name  } = raw_user;
+    const sanitized_user = {email,display_name};
+    return sanitized_user;
+}
 
 const getUser = async (req,res) =>{
     let {email,password} = req.body;
-    const user = await userDbService.getUser(email,password)
-    res.json(user);
+    const raw_user = await userDbService.getUser(email);
+    if(raw_user!=null){
+        let cmp_res = await bcrypt.compare(password,raw_user.password);
+        if(cmp_res){
+            const sanitized_user = filtered_user(raw_user);
+            res.json(sanitized_user);
+            return;
+        }
+        else{
+            res.send({error:"Wrong Password"});
+        }
+    }
+    else{
+        res.send({error:"User Not Found"});
+    }
 }    
 
 const createUser = async (req,res) => {
     let {email,password} = req.body;
-    const newUser = await userDbService.createUser(email,password);
-    res.json(newUser);
+    const userExist = await userDbService.getUser(email);
+    if(userExist){
+        res.send({error:"a User with this email already exist. Send a recovery email?"});
+        return;
+    }
+    let hashed_pass = await bcrypt.hash(password,salt_rounds);
+    if(hashed_pass==null){
+        res.send({error:"bcrypt hash failed"});
+        return;
+    }
+    const newUser = await userDbService.createUser(email,hashed_pass);
+    if(newUser != {}){
+        const sanitized_user = filtered_user(newUser);
+        res.json(sanitized_user);
+    }
+    else{
+        res.send({error:"ERROR: can't create user"});
+    }
 };
 
 const getUsers = async (req,res) => {
