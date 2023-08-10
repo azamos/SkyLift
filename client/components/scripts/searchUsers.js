5ab162ab619ceabf95ad3ae9857f8d27cc6d79cb
amos zohar <azamoszohar@gmail.com>
2023-08-10 11:54:07 +0300

    made it so that search user will load the userpage and populate it with real data. of course, there is much work remains to be done

--- client/components/scripts/searchUsers.js
+++ client/components/scripts/searchUsers.js
@@ -10,6 +10,14 @@ const search = e => {
             console.log(res);
             if (!res.error) {
                 $("#searchUser-input").val("");
+                $('#main-component-container').load(`${views_path}/userpage.html`,()=>{
+                    $('#userName').text(res.email);
+                    $('#userEmail').text(state.display_name);
+                    $('#totalMiles').text(res.total_miles);
+                    $('#futureDealsSum').text("1");
+                    $('#pastFlights').text("1");
+                    $('#cartSum').text("1");  
+                })
             }
         })
         .catch(err => console.log(err));
