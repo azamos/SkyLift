c0c9df08f92c3e79a08bc67dc1428a406d481bca
Omer Maoz <omermaoz@Omers-MacBook-Air.local>
2023-08-16 02:50:27 +0300

    fixed the cart->login issue and change style for some items

--- client/components/scripts/login.js
+++ client/components/scripts/login.js
@@ -19,15 +19,8 @@ const login = e => {
             if (!res.error) {
                 $("#login-email-input").val("");
                 $("#login-password-input").val("");
-
+                $("#userIdentitySpan").text(`User: ${res.name}`).css('background-color','green');
                 headers.set('Authorization',res.token);
-                if(res.isAdmin){
-                    $("#userIdentitySpan").text(`User: ${res.name}`).css('background-color','red');
-                }
-                else{
-                    $("#userIdentitySpan").text(`User: ${res.name}`).css('background-color','green');
-                }
-
                 state.user = res.email;
                 state.name = res.name;
                 state.token = res.token;
