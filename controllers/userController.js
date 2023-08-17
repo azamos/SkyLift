c4f0a0ec208e584283e10735baa32240a21c9b11
amos zohar <azamoszohar@gmail.com>
2023-08-17 18:46:18 +0300

    added commentery for team, and added some serverside input validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,33 +2,62 @@ const userDbService = require('../services/userDbService');
 const tokenDbService = require('../services/tokenDbService');
 const flightDbService = require('../services/flightDbService');
 const utils = require('../services/utils');
-const { is_authorized } = utils;
+const { is_authorized, emailSyntaxIsValid } = utils;
 const bcrypt = require('bcrypt');
 const salt_rounds = 12;
 
-/* internal function, must NOT export. */
-const filtered_user = raw_user => {
-    const { email, display_name } = raw_user;
-    const sanitized_user = { email, display_name };
-    return sanitized_user;
-}
+/*NOTE FOR TEAM:
+    This is the Controller for the Users, which handles ALL requests
+    that relate to users.
+    Each function here uses different operations, but all of them have similar responsibilites:
+        1. Checks that whoever made a request is allowed to do so(called authorization).Note: not all requests require an authorization.
+        2. Makes sure that the requests themselve are valid(for example,
+            if a user registers, it will reach the function createUser I wrote bellow. In it, we make sure
+            that the email is in a valid format, that the password isn't an empty string)
+        3.In case everything is Kosher, the function must(according to the type of operation reqired)
+        either bring some data from the DB(Data-Base), or create a new instance, delete an existing one,
+        or update an instance. All of this is done by using the relevant DBservices.
+        IT is important to understand: the DBservices function's responsibility IS NOT to check if the data is valid,
+        and if it is an authorized request. They simply expect the correct format of parameters, and perform the required
+        operation on the database.
 
-async function getUsersFlights(user_mail){
+    REMINDER:(CRUD)
+        The project requirements dictate that for each entitySet/Model, we must provide the following functions:
+        1. LIST: return a list of all instances(for example, return a list of all users).This is a READ operation.
+        2. SEARCH users by a few paramaters(for example: email, name, some other field...).This is a READ operation.
+        3.DELETE a user(we will go by email for now); This is a DELETE operation
+        4.CREATE a user; This is a CREATE operation.
+        5.UPDATE a user; This is a UPDATE operation.
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*think of this as a private function, for now I do not want to export it.
+Used to bring all the flights related to the user. */
+async function getUsersFlights(user_mail) {
     const user = await userDbService.findUserByMail(user_mail);
     const past_flights = await flightDbService.getFlightsByIdArr(user.past_flights);
     const cart = await flightDbService.getFlightsByIdArr(user.cart);
     const future_flights = await flightDbService.getFlightsByIdArr(user.future_flights);
     user.password = null;
     return {
-        user,past_flights,cart,future_flights
+        user, past_flights, cart, future_flights
     }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* The function bellow is used if someone wants to view his OWN profile page,
+or if an admin wishes to watch any other user's profile page(also called Account page).*/
 
 /* for authorized users only: either the user himself, or an admin. */
+/* Works fine, but try to find edge cases I might have missed */
 const getUserData = async (req, res) => {
     if (req.cookies && req.cookies.token) {
         let { email } = req.body;
-        const authorizedFlag = await is_authorized(req.cookies.token,email);
+        if (emailSyntaxIsValid(email) == false) {
+            res.send('Not a valid email input. Try again with a valid email');
+            return;
+        }
+        const authorizedFlag = await is_authorized(req.cookies.token, email);
         if (authorizedFlag) {
             const user_data = await userDbService.findUserByMail(email);
             if (user_data) {
@@ -43,10 +72,21 @@ const getUserData = async (req, res) => {
     }
     res.send({ error: 'unauthorized user' });
 }
-//TODO: continue
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* As the name suggests, this function SHOULD be called whenever an existing user tries to call in. */
+/* Works fine, but try to find edge cases I might have missed */
 const userLogin = async (req, res) => {
     let { email, password } = req.body;
-    
+    if (emailSyntaxIsValid(email) == false) {
+        res.send('Not a valid email input. Try again with a valid email');
+        return;
+    }
+    if (password.trim() == "") {
+        res.send('empty password');
+        return;
+    }
+
     /* if there is an authorization header attached, it means that a user
     is already logged in, or have an expired token, in the browser.
     Thus I must invalidate the previous token if it is valid, otherwise, nothing? */
@@ -75,7 +115,7 @@ const userLogin = async (req, res) => {
             res.cookie('token', key, {
                 httpOnly: true,
                 sameSite: 'strict'
-            }).json({ token:key, email, name });
+            }).json({ token: key, email, name });
             return;
         }
         else {
@@ -86,7 +126,9 @@ const userLogin = async (req, res) => {
         res.send({ error: "User Not Found" });
     }
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* FOR now, I only use this function in case a user is already logged in, and he wishes to CREATE another user.
+Thus, we must discconect his previous user, and remove all relevant authorizations */
 const logOff = async (req, res) => {
     if (req.cookies && req.cookies.token) {
         const token = await tokenDbService.getToken(req.cookies.token);
@@ -96,8 +138,18 @@ const logOff = async (req, res) => {
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Works fine, but try to find edge cases I might have missed */
 const createUser = async (req, res) => {
     let { email, password, full_name, phone_number } = req.body;
+    if (emailSyntaxIsValid(email) == false) {
+        res.send('Not a valid email input. Try again with a valid email');
+        return;
+    }
+    if (password.trim() == "") {
+        res.send('empty password');
+        return;
+    }
     const userExist = await userDbService.findUserByMail(email);
     if (userExist) {
         res.send({ error: "a User with this email already exist. Send a recovery email?" });
@@ -119,8 +171,8 @@ const createUser = async (req, res) => {
         await tokenDbService.createToken(token_id, email);
         res.cookie('token', key, {
             httpOnly: true,
-            sameSite:'strict'
-        }).json({ token:key, email });
+            sameSite: 'strict'
+        }).json({ token: key, email });
         return;
     }
     else {
@@ -128,9 +180,11 @@ const createUser = async (req, res) => {
     }
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* WORKS fine when you are logged in with an admin, as is required. */
 const getUsersList = async (req, res) => {
     if (req.cookies && req.cookies.token) {
-        const authorizedFlag = await is_authorized(req.cookies.token, email);
+        const authorizedFlag = await is_authorized(req.cookies.token);
         if (!authorizedFlag) {
             res.send({ error: 'unauthorized request. If you are allowed to view this, login again' });
             return;
@@ -145,16 +199,9 @@ const getUsersList = async (req, res) => {
 };
 
 
-const userLoginByEmail = async (req, res) => {
-    const { email } = req.params;
-    const userIdentified = await userDbService.userLoginByEmail(email);
-    if (!userIdentified) {
-        res.send(`failed to find flight with id = ${email} to be retrieved from the database.`);
-    }
-    res.send(userIdentified);
-};
-
-const updateUserData = async (req, res) => {//will reach here with a get request, so extract data from req.params
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*WARNING: testing is required */
+const updateUser = async (req, res) => {
     const { email, newData } = req.params;
     const userToBeUpdated = await userDbService.updateUserData(email, newData);
     if (!userToBeUpdated) {
@@ -163,9 +210,12 @@ const updateUserData = async (req, res) => {//will reach here with a get request
     res.json(userToBeUpdated);
 };
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*WARNING: testing is required */
 const deleteUser = async (req, res) => await userDbService.deleteUser(req.params.id);
 
-module.exports = { userLogin, createUser, getUserData, getUsersList};
+module.exports = { userLogin, createUser, getUserData, getUsersList };
 
 
 
--- services/utils.js
+++ services/utils.js
@@ -1,5 +1,8 @@
 const tokenDbService = require('./tokenDbService');
 const bcrypt = require('bcrypt');
+//Basic email regex detection.
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailSyntaxIsValid = email_address => EMAIL_REGEX.test(email_address);
 
 const is_authorized = async (key,email = null) =>{
     let token_id = `${key}${process.env.SECRET}`;
@@ -11,4 +14,4 @@ const is_authorized = async (key,email = null) =>{
     return email && token.user == email || token.authorization == 'admin';
 }
 
-module.exports = {is_authorized};
+module.exports = {is_authorized,emailSyntaxIsValid};
