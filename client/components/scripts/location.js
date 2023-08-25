0e7ddefb57c12c559da59f31b443cc6a8d610196
Omer Maoz <omermaoz1998@gmail.com>
2023-08-25 15:30:45 +0300

    added token html and change location html

--- client/components/scripts/location.js
+++ client/components/scripts/location.js
@@ -1,15 +1,14 @@
 const generateLoctionHTML = (locationModelInstanc) =>{
-    console.log(locationModelInstanc);
     let htmlRef = $("#location-template").clone();
     
     htmlRef.attr('style',"background-color: rgb(224, 224, 217)");
-    htmlRef.children('#location-details').children('.card').children('.card-body').children('#cityName').text(`City Name: ${locationModelInstanc.cityName}`);
-    htmlRef.children('#location-details').children('.card').children('.card-body').children('#country-name').text(`Country: ${locationModelInstanc.country}`);
-    htmlRef.children('#location-details').children('.card').children('.card-body').children('#airport').text(`Airport: ${locationModelInstanc.airport}`);
-    htmlRef.children('#location-details').children('.card').children('.card-body').children('#flight-type').text(`International: ${locationModelInstanc.international}`);
-
-    htmlRef.children('#location-details').children('.card').children('.card-body').children('.btn-outline-danger-delete').on('click',async()=>{
-
+    htmlRef.children('.card-body').children('#country-name').text(`Country: ${locationModelInstanc.country}`);
+    htmlRef.children('.card-body').children('#cityName').text(`City Name: ${locationModelInstanc.cityName}`);
+    htmlRef.children('.card-body').children('#airport').text(`Airport: ${locationModelInstanc.airport}`);
+    htmlRef.children('.card-body').children('#international').text(`International: ${locationModelInstanc.international}`);
+    htmlRef.children('.card-head').children('.card-img-location').attr('src',locationModelInstanc.imageUrl);
+    
+    htmlRef.children('.card-body').children('.btn-outline-danger').on('click',async()=>{
         
 
     });
