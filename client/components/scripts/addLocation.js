e195976db709075641744f52f5334506610f6fc9
Shahar Wissbecker <shahar939@gmail.com>
2023-08-29 16:50:08 +0300

    validations git status

--- client/components/scripts/addLocation.js
+++ client/components/scripts/addLocation.js
@@ -1,13 +1,40 @@
 const addLocation = async e => {
     e.preventDefault();
-    const cityName_input = $("#cityName-input");
-    const country_input = $("#country-input");
-    const airport_input = $("#airport-input");
+    let cityName_input = $("#cityName-input").val();
+    let country_input = $("#country-input").val();
+    let airport_input = $("#airport-input").val();
+    
+    if(cityName_input == "" || country_input == "" ||  airport_input==""){
+        alert("Please Fill All Fileds");
+        return;
+    }
+
+    let check = checkInput(cityName_input);
+    if(!check){
+        alert("City Name is not valid!");
+        return;
+    }
+    check = checkInput(country_input);
+    if(!check){
+        alert("country Name is not valid!");
+        return;
+    }
+    check = checkInput(airport_input);
+    if(!check){
+        alert("airport Name is not valid!");
+        return;
+    }
+    cityName_input = formatInput(cityName_input);
+    country_input = formatInput(country_input);
+    airport_input = formatInput(airport_input , true);
+    
     fetch(`${url}/locations`,{
         method:'POST',
         headers,
-        body:JSON.stringify({'cityName':cityName_input.val(),'country':country_input.val(),'airport':airport_input.val()})
-    }).then(res=>res.json()).then(res=>console.log(res)).catch(e=>console.error(e))
+        body:JSON.stringify({'cityName':cityName_input,'country':country_input,'airport':airport_input})
+    })
+    .then(res=>res.json())
+    .catch(e=>console.error(e))
     loadMainComponent('addLocation');
 }
 
@@ -28,4 +55,53 @@ const loadLocations = async () => {
         });
     })
     .catch(err=>console.log(err));
+}
+
+
+
+function formatInput(inputString , isAirport = false) {
+    // Remove leading and trailing spaces
+    inputString = inputString.trim();
+
+    // Split the input string into words
+    var words = inputString.split(/\s+/);
+
+    // Format each word: capitalize first letter, make the rest lowercase
+    for (var i = 0; i < words.length; i++) {
+        if(words[i].toUpperCase() === "USA"){
+            words[i] = words[i].toUpperCase(); 
+        }
+        else if (!isAirport) {
+            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+        } else {
+            words[i] = words[i].toUpperCase(); // Keep 3 letters airport capital
+        }
+    }
+
+    // Join the formatted words to create the formatted input
+    var formattedInput = words.join(" ");
+
+    return formattedInput;
+}
+
+function checkInput(inputString) {
+    // Check if the input has at least 2 letters
+    if (inputString.length < 2) {
+        return false;
+    }
+
+    // Format the input
+    var formattedInput = formatInput(inputString);
+
+    // Check if the formatted input follows the validation rules
+    var words = formattedInput.split(/\s+/);
+
+    // Check if each word has at least two letters
+    for (var i = 0; i < words.length; i++) {
+        if (words[i].length < 2) {
+            return false;
+        }
+    }
+
+    return true;
 }
